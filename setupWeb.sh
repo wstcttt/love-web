5c16185ec35db982a55ed0751af25cef4532cc28
wstcttt <wstcttt@gmail.com>
2017-06-26 20:14:46 +0800

    make it to cp all files to /www

--- setupWeb.sh
+++ setupWeb.sh
@@ -1,12 +1,15 @@
 #for centOS6 64bit
 wget http://pypi.python.org/packages/source/s/setuptools/setuptools-2.0.tar.gz
 tar zxvf setuptools-2.0.tar.gz
+rm -f setuptools-2.0.tar.gz
 cd setuptools-2.0
 python setup.py build
 python setup.py install
+cd ..
+rm -rf setuptools-2.0
 easy_install flask
 easy_install gunicorn
 rpm -ivh http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm
 yum install nginx
-
-
+mkdir -p /www
+cp -r * /www
